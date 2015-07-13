6b202a8cb231f9c98bb930167460f7521762be38
olegccc <oleg@gordeev.cz>
2015-07-14 02:42:02 +0300

    Updated spec to wait for visible

--- test/specs/NewUserCreationSpec.ts
+++ test/specs/NewUserCreationSpec.ts
@@ -17,33 +17,28 @@ describe('When we enter new user fields and submit the dialog', () => {
         var userName:string = 'username';
         var userPassword = 'pwd';
         var userEmail:string = 'user@email.com';
-        var userBirthday: string = '01012001';
 
-        browser.wait(() => {
-            return browser.isElementPresent(by.css('md-content [name=name]'));
-        }, 10000).then(() => {
-            var name = element(by.css('md-content [name=name]'));
-            name.sendKeys(userName);
-            var password = element(by.css('md-content [name=password]'));
-            var passwordRepeat = element(by.css('md-content [name="password$Repeat"]'));
-            password.sendKeys(userPassword);
-            passwordRepeat.sendKeys(userPassword);
-            var email = element(by.css('md-content [name=email]'));
-            email.sendKeys(userEmail);
-            var okButton = element(by.css('.md-actions button.md-primary'));
-            okButton.click();
+        var name = element(by.css('md-content [name=name]'));
+        browser.wait(protractor.until.elementIsVisible(name), 10000);
+        name.sendKeys(userName);
+        var password = element(by.css('md-content [name=password]'));
+        var passwordRepeat = element(by.css('md-content [name="password$Repeat"]'));
+        password.sendKeys(userPassword);
+        passwordRepeat.sendKeys(userPassword);
+        var email = element(by.css('md-content [name=email]'));
+        email.sendKeys(userEmail);
+        var okButton = element(by.css('.md-actions button.md-primary'));
+        okButton.click();
 
-            var returnedObject = element(by.id('returned-object'));
-            browser.wait(protractor.until.elementTextMatches(returnedObject, /.+/), 50000);
+        var returnedObject = element(by.id('returned-object'));
+        browser.wait(protractor.until.elementTextMatches(returnedObject, /.+/), 50000);
 
-            returnedObject.getText().then((text: string) => {
-                var newUser = JSON.parse(text);
-                expect(newUser.name).toBe(userName);
-                expect(newUser.email).toBe(userEmail);
-                expect(newUser.password).toBe(userPassword);
-                done();
-            });
+        returnedObject.getText().then((text: string) => {
+            var newUser = JSON.parse(text);
+            expect(newUser.name).toBe(userName);
+            expect(newUser.email).toBe(userEmail);
+            expect(newUser.password).toBe(userPassword);
+            done();
         });
-
     });
 });
